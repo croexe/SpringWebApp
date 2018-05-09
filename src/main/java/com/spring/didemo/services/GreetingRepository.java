6e8f454934604dbe6a68fabf5018be7e63bab178
Irinej Stjepanović <irinejs@gmail.com>
2018-05-09 17:28:47 +0200

    Setting up Factory

---
+++ src/main/java/com/spring/didemo/services/GreetingRepository.java
@@ -0,0 +1,10 @@
+package com.spring.didemo.services;
+
+public interface GreetingRepository {
+
+    String getEnglishGreeting();
+
+    String getSpanishGreeting();
+
+    String getGermanGreeting();
+}
